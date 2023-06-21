38de51fdef1ee7a5e83fa10ac7a0991eca16d541
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-21 17:55:03 +0000

    Added route from express_server to urls_new

--- express_server.js
+++ express_server.js
@@ -23,11 +23,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 });
 
+//route to urls_new
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 //route to urls_show
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

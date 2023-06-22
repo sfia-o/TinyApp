ba3adcdc59f2dd1efb0f996575f9c9d31ad18b1f
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-22 20:39:02 +0000

    Fixed update post request and refactored organizational comments

--- express_server.js
+++ express_server.js
@@ -34,7 +34,7 @@ function generateRandomString() {
 
 
 /**
- *  ??   R O U T E S -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ *  R O U T E S -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
  */
 
 //route to root
@@ -52,18 +52,18 @@ app.get("/urls.json", (req, res) => {
  */
 
 
-//route to urls_index
+//List All URLs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
 });
 
-//route to urls_new
+//Page to Create New URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 })
 
-//route to urls_show
+//Page to View New URL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { id: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
@@ -74,7 +74,7 @@ app.get("/urls/:shortURL", (req, res) => {
  *  A P I   R O U T E S -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
  */
 
-//post to /urls
+//Create New URL
 app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
@@ -82,31 +82,34 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-//route to longURL through the shortURL
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
-//remove url resource
+//Delete URL
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 })
 
-//edit route
+//Route to Edit URL
 app.post("/urls/:shortURL/edit", (req, res) => {
   const shortURL = req.params.shortURL
   res.redirect(`/urls/${shortURL}`);
 })
 
-//update resource
+//Edit / Update URL
 app.post("/urls/:shortURL/submit", (req, res) => {
-  const shortURL = req.params.shortURL;
   const newURL = req.body.newURL;
-  urlDatabase[shortURL] = newURL; 
+  const shortURL = req.params.shortURL;
+  
+  if (urlDatabase[shortURL]) {
+    urlDatabase[shortURL] = newURL;
+  }
   res.redirect("/urls");
 })
+
+//Route to longURL through the shortURL
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 
 /**

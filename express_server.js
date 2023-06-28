be5edbcec6e6e8c2c476d72ef22a950b3d6801a3
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-28 01:18:59 +0000

    Linted and edited buttons' style on urls_index

--- express_server.js
+++ express_server.js
@@ -21,7 +21,7 @@ const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
     userID: "aJ48lW",
-    },
+  },
   "i3BoGr": {
     longURL: "https://www.google.ca",
     userID: "aJ48lW",
@@ -34,7 +34,7 @@ const users = {
     email: "user@example.com",
     password: "purple-monkey-dinosaur",
   }
-}
+};
 
 //get shortURL
 function generateRandomString() {
@@ -46,19 +46,19 @@ function generateRandomString() {
     shortURL += characters.charAt(randomChar);
   }
   return shortURL;
-};
+}
 
 //finding user by email
 function userEmailExists(email, users) {
-  let user 
+  let user;
 
   for (const id in users) {
-      if (users[id].email === email) {
+    if (users[id].email === email) {
       user = users[id];
-      }
+    }
   }
   return user;
-};
+}
 
 //list urls for user
 function urlsForUser(id, urlDatabase) {
@@ -66,7 +66,7 @@ function urlsForUser(id, urlDatabase) {
 
   for (const element in urlDatabase) {
     if (element === id) {
-      savedURLS
+      savedURLS;
     }
   }
 }
@@ -114,7 +114,7 @@ app.get("/urls", (req, res) => {
   
   //Condition to deny access to anyone not found on user database
   if (!userID) {
-    res.status(403).send("Access Denied: You don't have permission, please create account and log in before proceeding")
+    res.status(403).send("Access Denied: You don't have permission, please create account and log in before proceeding");
   }
   
   res.render("urls_index", templateVars);
@@ -128,9 +128,9 @@ app.get("/urls/new", (req, res) => {
 
   //Condition to only allow logged in users to create new url, otherwise redirect to log in page
   if (userID) {
-    res.render("urls_new", { user })
+    res.render("urls_new", { user });
   } else {
-    res.render("login", { user: null })
+    res.render("login", { user: null });
   }
 });
 
@@ -141,10 +141,10 @@ app.get("/urls/:id", (req, res) => {
 
   const user = users[userID];
   const id = req.params.id;
-  const longURL= urlDatabase[id].longURL;
+  const longURL = urlDatabase[id].longURL;
 
   if (!urlDatabase[id]) {
-    res.status(404).send("The url you are looking for does not exist")
+    res.status(404).send("The url you are looking for does not exist");
   }
 
   const templateVars = { id, longURL, user };
@@ -157,13 +157,13 @@ app.get("/urls/:id", (req, res) => {
 app.get("/register", (req, res) => {
   const userID = req.cookies.user_id;
   
-  //Condition to redirect logged in users to urls page 
+  //Condition to redirect logged in users to urls page
   if (userID) {
-    res.redirect("/urls")
+    res.redirect("/urls");
   } else {
-    res.render("register", { user: null })
+    res.render("register", { user: null });
   }
-})
+});
 
 
 //Login
@@ -172,11 +172,11 @@ app.get("/login", (req, res) => {
   
   //Condition to redirect logged in users to urls page
   if (userID) {
-    res.redirect("/urls")
+    res.redirect("/urls");
   } else {
-    res.render("login", { user: null })
+    res.render("login", { user: null });
   }
-})
+});
 
 
 /**
@@ -189,7 +189,7 @@ app.post("/urls", (req, res) => {
 
   //Condition to deny access to anyone not found on user database
   if (!userID) {
-    res.status(403).send("Access Denied: You don't have permission, please create account before proceeding")
+    res.status(403).send("Access Denied: You don't have permission, please create account before proceeding");
   }
   
   //Declare variables to store url submitted in form and another to store a randomly generated ID
@@ -200,7 +200,7 @@ app.post("/urls", (req, res) => {
   urlDatabase[id] = {
     longURL,
     userID //userID from cookie
-  }
+  };
   
   res.redirect(`/urls/${id}`);
 });
@@ -246,15 +246,15 @@ app.post("/login", (req, res) => {
   
   //Check if user exists in database
   if (!userEmailExists(email, users)) {
-    res.status(403).send("User not found")
-  } 
+    res.status(403).send("User not found");
+  }
 
   //Declare user variable to store the found user
   const user = userEmailExists(email, users);
 
   //Check if found users password corresponds to input password
   if (user.password !== password) {
-    res.status(403).send("Invalid Password")
+    res.status(403).send("Invalid Password");
   }
   
   //Set cookie to corresponding userID
@@ -273,27 +273,27 @@ app.post("/logout", (req, res) => {
 //Register New User
 app.post("/register", (req, res) => {
   
-  const email = req.body.email; 
+  const email = req.body.email;
   const password = req.body.password;
   
   //Denied registration using empty values
   if (email === '' ||  password === '') {
-    res.status(400).send('Invalid Input')
+    res.status(400).send('Invalid Input');
     return;
   }
 
   //Denied duplicate email registrations
   if (userEmailExists(email, users)) {
-    res.status(400).send("Email is already registered")
+    res.status(400).send("Email is already registered");
   }
 
   //Generate a new ID for the new user and add it to database
   const id = generateRandomString();
   users[id] = { id, email, password };
   
-  res.cookie('user_id', id)
-  res.redirect("/urls")
-})
+  res.cookie('user_id', id);
+  res.redirect("/urls");
+});
 
 
 /**

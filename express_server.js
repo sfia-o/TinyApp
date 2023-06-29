50693c2179c7dda701b148528d40b1f2de9226df
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-29 13:55:43 -0700

    Create helper function file, move functions. Export and import back into server.

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const cookieSession = require('cookie-session');
 const bcrypt = require("bcryptjs");
+const { generateRandomString, userEmailExists, urlsForUser } = require("./helperFunctions.js")
 
 
 /**
@@ -55,49 +56,6 @@ const users = {
     password: "purple",
   }
 };
-
-//get shortURL
-function generateRandomString() {
-  let characters = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let shortURL = '';
-
-  for (let i = 0; i < 6; i++) {
-    let randomChar = Math.floor(Math.random() * characters.length);
-    shortURL += characters.charAt(randomChar);
-  }
-  return shortURL;
-}
-
-//finding user by email
-function userEmailExists(email, users) {
-  let user;
-
-  for (const id in users) {
-    if (users[id].email === email) {
-      user = users[id];
-    }
-  }
-  return user;
-}
-
-//list urls for user
-function urlsForUser(id, urlDatabase) {
-  //Declare new object
-  let savedURLS = {};
-  
-  //Loop through database
-  for (const url in urlDatabase) {
-
-    //check if id matches userID
-    if (urlDatabase[url].userID === id) {
-
-      //keep that as a savedURLS
-      savedURLS[url] = urlDatabase[url]
-    }
-  }
-  return savedURLS;
-}
-
 
 /**
  *  R O U T E S -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
---
+++ helperFunctions.js
@@ -0,0 +1,51 @@
+/**
+ * H E L P E R   F U N C T I O N S ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ */
+
+//get shortURL
+function generateRandomString() {
+  let characters = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let shortURL = '';
+
+  for (let i = 0; i < 6; i++) {
+    let randomChar = Math.floor(Math.random() * characters.length);
+    shortURL += characters.charAt(randomChar);
+  }
+  return shortURL;
+}
+
+//finding user by email
+function userEmailExists(email, users) {
+  let user;
+
+  for (const id in users) {
+    if (users[id].email === email) {
+      user = users[id];
+    }
+  }
+  return user;
+}
+
+//list urls for user
+function urlsForUser(id, urlDatabase) {
+  //Declare new object
+  let savedURLS = {};
+  
+  //Loop through database
+  for (const url in urlDatabase) {
+
+    //check if id matches userID
+    if (urlDatabase[url].userID === id) {
+
+      //keep that as a savedURLS
+      savedURLS[url] = urlDatabase[url]
+    }
+  }
+  return savedURLS;
+}
+
+module.exports = { 
+  generateRandomString, 
+  userEmailExists, 
+  urlsForUser 
+};
